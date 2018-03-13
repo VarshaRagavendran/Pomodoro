059f4a1badcadaa7783c18519037c786790d22a2
Varsha <varsha.ragavendran@gmail.com>
2018-03-13 00:00:09 -0400

    added main storyboard
    - added new main page storyboard
    - pomodoro option segue’s into timer

---
+++ Pomodoro/mainPageController.cs
@@ -0,0 +1,18 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Pomodoro
+{
+    public partial class mainPageController : UIViewController
+    {
+        public mainPageController (IntPtr handle) : base (handle)
+        {}
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+           // this.NavigationItem.SetHidesBackButton (true, false);
+            this.NavigationController.SetNavigationBarHidden(true, false);
+        }
+    }
+}
